ed241379b8cf4bafbb8816813f67f1caef107913
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 10:49:09 +0530

    understanding unassigment

--- src/script.js
+++ src/script.js
@@ -7,9 +7,17 @@ comment
 */
 var myName;
 var a;
-a=7;
+a = 7;
 var b;
-b=a;
+b = a;
 var a = 9;
 var myFirstName = "Akanksha";
 var myLastName = "Dixit";
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
