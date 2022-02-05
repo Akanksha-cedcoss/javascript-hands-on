4d00a807545eae4790ee66ed4dd6add16fe442d9
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 11:40:35 +0530

    str index

--- src/script.js
+++ src/script.js
@@ -11,3 +11,9 @@ const lastName = "Lovelace";
 
 lastNameLength = lastName.length;
 console.log(lastNameLength);
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+firstLetterOfLastName = lastName[0];
+console.log(firstLetterOfLastName);
