02e8ee0eca2a01e4952cdac6fc6807f5168ad525
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 11:25:35 +0530

    string with quote

--- src/script.js
+++ src/script.js
@@ -5,13 +5,5 @@ a
 multiline
 comment
 */
-
-let a = 48;
-let b = 108;
-let c = 33;
-a  /= 12;
-b  /= 4;
-c  /= 11;
-console.log(a);
-console.log(b);
-console.log(c);
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+console.log(myStr);
