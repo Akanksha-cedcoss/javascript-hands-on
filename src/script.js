6e7d1a883b52a856c80832e227eceb79340c90c0
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 14:25:16 +0530

    Accessing Nested Objects

--- src/script.js
+++ src/script.js
@@ -571,3 +571,17 @@ const myMusic = [
     formats: ["a", "b", "b"],
   },
 ];
+//93
+const myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
