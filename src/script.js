1590d7415fc1740599cfb580109de331ed20a8c5
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 11:34:16 +0530

    concatenate string with +=

--- src/script.js
+++ src/script.js
@@ -5,5 +5,6 @@ a
 multiline
 comment
 */
-const myStr = "This is the start."+" This is the end.";
+let myStr='This is the first sentence.';
+myStr += ' This is the second sentence.';
 console.log(myStr);
