48cb568b083a34978a25ee25d98ad786da525e47
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 12:21:54 +0530

    task 55

--- src/script.js
+++ src/script.js
@@ -9,18 +9,14 @@ comment
 let myGlobal = 10;
 
 function fun1() {
-   oopsGlobal = 5;
-
-
+  oopsGlobal = 5;
 }
 function myLocalScope() {
-    // Only change code below this line
-  let myVar=5;
-    console.log('inside myLocalScope', myVar);
-  }
-  myLocalScope();
-  
-
+  // Only change code below this line
+  let myVar = 5;
+  console.log("inside myLocalScope", myVar);
+}
+myLocalScope();
 
 function fun2() {
   var output = "";
@@ -37,7 +33,7 @@ const outerWear = "T-Shirt";
 
 function myOutfit() {
   // Only change code below this line
-const outerWear='sweater';
+  const outerWear = "sweater";
   // Only change code above this line
   return outerWear;
 }
@@ -65,3 +61,19 @@ function timesFive(a) {
   return a * 5;
 }
 timesFive(5);
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
