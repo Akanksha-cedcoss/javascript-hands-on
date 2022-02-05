61e83434a507e6b1ceb121a39b2ced511bd92928
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 11:09:38 +0530

    decimal multiply

--- src/script.js
+++ src/script.js
@@ -5,19 +5,8 @@ a
 multiline
 comment
 */
-const sum = 10 + 10;
-console.log(sum);
-const difference = 45 - 33;
-console.log(difference);
-const product = 8 * 10;
-console.log(product);
-const quotient = 66 / 33;
-console.log(quotient);
-let myVar = 87;
-myVar++;
-console.log(myVar);
-let myVar2 = 11;
-myVar2--;
-console.log(myVar2);
+
 var myDecimal = 5.5;
 console.log(myDecimal);
+const product = 2.0 * 2.5;
+console.log(product);
