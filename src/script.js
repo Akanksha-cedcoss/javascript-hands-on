a8c902f679a7eff7b80349a00d2c78a034885260
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 11:19:11 +0530

    compound assignment multiply

--- src/script.js
+++ src/script.js
@@ -6,13 +6,12 @@ multiline
 comment
 */
 
-let a = 11;
-let b = 9;
-let c = 3;
-
-a -= 6;
-b -= 15;
-c -= 1;
+let a = 5;
+let b = 12;
+let c = 4.6;
+a *= 5;
+b *=3;
+c *= 10;
 console.log(a);
 console.log(b);
 console.log(c);
