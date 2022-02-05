61fbe89ac69fb01d336d19a6bc7be2b7dcddb4d9
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 13:11:43 +0530

    Gold code

--- src/script.js
+++ src/script.js
@@ -291,3 +291,36 @@ function testSize(num) {
 }
 
 console.log(testSize(7));
+//75
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  }
+  // Only change code above this line
+}
+
+console.log(golfScore(5, 4));
