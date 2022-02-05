c0d5446d11f3ffdd41bc825c53e76570cff531c6
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 10:58:31 +0530

    add two num

--- src/script.js
+++ src/script.js
@@ -5,26 +5,8 @@ a
 multiline
 comment
 */
-var myName;
-var a;
-a = 7;
-var b;
-b = a;
-var a = 9;
-var myFirstName = "Akanksha";
-var myLastName = "Dixit";
-
-var a = 5;
-var b = 10;
-var c = "I am a";
-
-a = a + 1;
-b = b + 5;
-c = c + " String!";
-
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+const sum = 10 + 10;
+console.log(sum);
 
 studlyCapVar = 10;
 properCamelCase = "A String";
