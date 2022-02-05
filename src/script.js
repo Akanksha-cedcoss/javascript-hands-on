b91cedb50f1e8d508be288b238ec5adfec7c933f
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 12:54:10 +0530

    Comparisons with the Logical And Operator

--- src/script.js
+++ src/script.js
@@ -207,3 +207,16 @@ function testLessOrEqual(val) {
 }
 
 console.log(testLessOrEqual(10));
+//69
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+console.log(testLogicalAnd(10));
