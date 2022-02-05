fd2f00302b58186e0a35d584b8a22654bdcff3a3
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 13:06:12 +0530

    Chaining If Else Statements

--- src/script.js
+++ src/script.js
@@ -272,3 +272,22 @@ function orderMyLogic(val) {
 }
 
 console.log(orderMyLogic(7));
+//74
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+
+  // Only change code above this line
+}
+
+console.log(testSize(7));
