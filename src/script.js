6d4d454689fcf2ab374e515e1aa219bad3b72566
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 13:22:21 +0530

    Selecting from Many Options with Switch Statements

--- src/script.js
+++ src/script.js
@@ -347,3 +347,26 @@ function caseInSwitch(val) {
 }
 
 console.log(caseInSwitch(1));
+//77
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+console.log(switchOfStuff(1));
