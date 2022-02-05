f9d1dd636cf26255eddfb047aea18af36b81cefd
akanksha-cedcoss <akankshadixit@cedcoss.com>
2022-02-05 11:13:06 +0530

    decimal divide

--- src/script.js
+++ src/script.js
@@ -10,3 +10,7 @@ var myDecimal = 5.5;
 console.log(myDecimal);
 const product = 2.0 * 2.5;
 console.log(product);
+const quotient = 4.4/ 2.0;
+console.log(quotient);
+const remainder = 12%5;
+console.log(remainder);
